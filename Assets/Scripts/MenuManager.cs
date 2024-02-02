07c3ceb68fd798ee79677990b0b36c5b960ea265
PracticalNoodle7 <156913534+PracticalNoodle7@users.noreply.github.com>
2024-02-02 22:43:52 +0000

    Adding in Buildings
    Needs more work just the building added and simple collision nothing more

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -8,6 +8,8 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject m_Controls_Settings_Panel;
     bool isCSPanelOpen = false;
+    public GameObject m_Inventory_Panel;
+    bool isInvPanelOpen = false;
 
    public void LoadLevel1()
    {
@@ -20,8 +22,21 @@ public class MenuManager : MonoBehaviour
         m_Controls_Settings_Panel.SetActive(isCSPanelOpen);
     }
     
+    public void OpenAndCloseInventoryPanel()
+    {
+        isInvPanelOpen = !isInvPanelOpen;
+        m_Inventory_Panel.SetActive(isInvPanelOpen);
+    }
     public void Quit()
     {
         Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            {
+                OpenAndCloseInventoryPanel();
+            }
     }
 }   
--- Assets/Scripts/TopDownCharacterController.cs
+++ Assets/Scripts/TopDownCharacterController.cs
@@ -84,6 +84,7 @@ public class TopDownCharacterController : MonoBehaviour
                 playerSpeed = 2f;
             }
                 
+            
         }
         else
         {
