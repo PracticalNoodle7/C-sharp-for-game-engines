054b8ad12f1c5b7f8e890601d9f82b2fd34ced00
PracticalNoodle7 <156913534+PracticalNoodle7@users.noreply.github.com>
2024-02-17 23:39:09 +0000

    Adding hotbar
    No where near done

---
+++ Assets/Scripts/HotbarManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HotbarManager : MonoBehaviour
+{
+    public void SwitchHotbarItem()
+    {
+
+    }
+}
